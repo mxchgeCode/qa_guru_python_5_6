a9f447ae2d0eeceaae38185cfe40ec3f97d6b7e9
mtx <m33wwe@gmail.com>
2023-04-20 21:53:55 +0300

    test_dark_theme_by_time minor fix

--- test_homework_6.py
+++ test_homework_6.py
@@ -9,6 +9,8 @@ def test_dark_theme_by_time():
 
     if current_time.hour < 7 or current_time.hour > 22:
         is_dark_theme = True
+    else:
+        is_dark_theme = False
     assert is_dark_theme is True
 
 
@@ -76,6 +78,7 @@ def test_readable_function():
     open_browser(browser_name="Chrome")
     go_to_companyname_homepage(page_url="https://companyname.com")
     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
+    assert 1==1
 
 
 def print_a_name(input1, input2, input3, k):
@@ -90,14 +93,17 @@ def print_a_name(input1, input2, input3, k):
 
 def open_browser(browser_name):
     actual_result = print_a_name(open_browser.__name__, browser_name, 0, 1)
+    print(actual_result)
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
     actual_result = print_a_name(go_to_companyname_homepage.__name__, page_url, 0, 2)
+    print(actual_result)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
     actual_result = print_a_name(find_registration_button_on_login_page.__name__, page_url, button_text, 3)
+    print(actual_result)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
